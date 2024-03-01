061578737b202eb7dccc969c2317282f0e8ecff8
Francesco Casetta <francesco.casetta@coolshop.it>
2024-03-01 08:30:17 +0100

    feat: styling

--- src/banner.js
+++ src/banner.js
@@ -1,3 +1,15 @@
+import styles from './banner.module.css';
+
 export const addBanner = (text) => {
-  document.querySelector('#content').textContent = text;
+  const el = document.querySelector('#content');
+  if (!el) {
+    return;
+  }
+
+  el.textContent = text;
+  el.classList.add(styles.content);
+
+  // import('./banner.module.css').then((styles) => {
+  //   el.classList.add(styles.content);
+  // });
 };
